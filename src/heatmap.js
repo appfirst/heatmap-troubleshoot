0ed27efed9b02fab536177c2cf5bf2dd2867a6ec
Matt Bullock <salty_cea_dog@hotmail.com>
2013-07-02 16:58:00 -0500

    added lots to heatmap, began processes/filtering

--- src/heatmap.js
+++ src/heatmap.js
@@ -4,8 +4,62 @@ d3.selection.prototype.moveToFront = function() {
   });
 };
 
+
+
+function filterServers() {
+	var options = d3.select(".combobox").select("form").select("select").selectAll("option")[0]
+	var rects = d3.select("#tiles").selectAll("*");
+	if(combobox.selection.selectedIndex != 0) {
+		if(options[combobox.selection.selectedIndex] == options[1]) { //avg_resp_time
+			var sorted = rects.sort(respSort);
+		}
+		else if(options[combobox.selection.selectedIndex] == options[2]) { //name
+			var sorted = rects.sort(nameSort);
+		}
+		sorted[0].forEach(function(e, i) {
+			d3.select(e).transition().duration(1000).attr("x", 40 * (i % columns) )
+				.attr("y", Math.floor(i / columns) * 40 )
+		});
+	}
+	
+	
+	function nameSort(a, b) {
+		if(a.topo.id.toLowerCase() < b.topo.id.toLowerCase()) {
+			return -1;
+		}
+		else if(a.topo.id.toLowerCase() == b.topo.id.toLowerCase()) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	
+	function respSort(a, b) {
+		if(isNaN(Number(a.topo.resp_avg)) && isNaN(Number(b.topo.resp_avg))) {
+			return 0;
+		}
+		else if(isNaN(Number(a.topo.resp_avg))) {
+			return 1;
+		}
+		else if(isNaN(Number(b.topo.resp_avg))) {
+			return -1;
+		}
+		else {
+			if(Number(a.topo.resp_avg) < Number(b.topo.resp_avg)) {
+				return -1;
+			}
+			else if(Number(a.topo.resp_avg) == Number(b.topo.resp_avg)) {
+				return 0;
+			}
+			else {
+				return 1;
+			}
+		}
+	}
+}
+
 var mainDiv = d3.select("body").append("div").attr("id", "mainDiv");
-var layer = 1
 var columns = 15
 var rows = 10
 var buckets = 11
@@ -16,7 +70,6 @@ var getFile = function(url, callback) {
 		dataType: "json",
 		crossDomain: true,
 		jsonp: "callback",
-		async: false,
 		success: function(data) {
 			callback(null, data)
 		},
@@ -50,101 +103,155 @@ function createHeatMap(topology, servers) {
 			})
 		}
 	})
-	// for(var i = serverstuff.length; i < 225; i++) {
-		// serverstuff.push( {topo: { id:"none" }, server: { nickname:"none" } } );
-	// }
 	createTiles(serverstuff);
 }
 
 function createTiles(s) {
-	function zoom(d, i) {
-		if(layer == 1) {
-			var thisRect;
-			var setValue = function(callback) {
-				d3.select("body").select("#mainDiv")
-					.select("svg").select("g")
-					.selectAll("rect")[0]
-					.forEach( function(e, j) {
-						if(i == j) {
-							thisRect = e;
-							callback(null, null);
-						}
-					})
-				}
-			function doAction() { 
-				d.on("click", null)
-					.transition().duration(1000)
-					.attr("width", 600)
-					.attr("height", 600)
-					.attr("x", 0)
-					.attr("y", 0)
-					
-				d3.select("body").select("#mainDiv").select("svg").select("g").selectAll("rect")
-					.filter(function(x, j) { 
-						return i != j;
-					})
-					.transition().duration(1000)
-					.attr("width", 600)
-					.attr("height", 600)
-					.attr("x", function() {
-						var dif = (this.x.baseVal.value - thisRect.x.baseVal.value)
-						var svgWidth = d3.select("body").select("#mainDiv")
-							.select("svg")[0][0].clientWidth
-						var rectWidth = thisRect.width.baseVal.value;
-						var mult = svgWidth / rectWidth;
-						return dif * mult;
-					})
-					.attr("y", function() {
-						var dif = (this.y.baseVal.value - thisRect.y.baseVal.value)
-						var svgHeight = d3.select("body").select("#mainDiv")
-							.select("svg")[0][0].clientHeight
-						var rectHeight = thisRect.height.baseVal.value;
-						var mult = svgHeight / rectHeight;
-						return dif * mult;
-					})
-				d3.select("body").select("#mainDiv").select("svg")
-					.append("g").attr("id", "back")
-					.append("rect")
-					.attr("x", d.attr("x"))
-					.attr("y", d.attr("y"))
-					.attr("width", 0)
-					.attr("height", 0)
-					.attr("opacity", 1)
-					.style("fill", "white")
-					.on("click", function() {
-						
-						$("#mainsvg").empty(); //THIS NEEDS TO CHANGE IT BACK TO LAYER 1
-					})
-					.classed("back", true)
-					.transition().duration(1000)
-					.attr("width", 50)
-					.attr("height", 30)
-					.attr("x", 20)
-					.attr("y", 20)
-				d3.select("#back").append("text")
-					.text("back")
-					.attr("x", d.attr("x"))
-					.attr("y", d.attr("y"))
-					.attr("width", 0)
-					.attr("height", 0)
-					.attr("font-size", 0)
-					.transition().duration(1000)
-					.attr("font-size", 12)
-					.attr("x", 45)
-					.attr("y", 40)
-				d.moveToFront;
-				layer = 2;
+	var oldx, oldy, thisRect;
+	function zoomIn(d, i) {
+		var setValue = function(callback) {
+			d3.select("body").select("#mainDiv")
+				.select("svg").select("g")
+				.selectAll("rect")[0]
+				.forEach( function(e, j) {
+					if(e.__data__ == d[0][0].__data__) {
+						thisRect = e;
+						oldx = thisRect.x.baseVal.value;
+						oldy = thisRect.y.baseVal.value;
+						callback();
+					}
+				})
 			}
-			queue(1)
-				.defer(setValue)
-				.awaitAll(doAction)
+		var doAction = function(callback) {
+			d.on("click", null)
+				.transition().duration(1000)
+				.attr("width", 600)
+				.attr("height", 600)
+				.attr("x", 0)
+				.attr("y", 0)
+				
+			d3.select("body").select("#mainDiv").select("svg").select("g").selectAll("rect")
+				.filter(function(x, j) {
+					return x.__data__ != d[0][0].__data__;
+				})
+				.transition().duration(1000)
+				.attr("width", 600)
+				.attr("height", 600)
+				.attr("x", function() {
+					var dif = (this.x.baseVal.value - thisRect.x.baseVal.value)
+					var svgWidth = d3.select("body").select("#mainDiv")
+						.select("svg")[0][0].clientWidth
+					var rectWidth = thisRect.width.baseVal.value;
+					var mult = svgWidth / rectWidth;
+					return dif * mult;
+				})
+				.attr("y", function() {
+					var dif = (this.y.baseVal.value - thisRect.y.baseVal.value)
+					var svgHeight = d3.select("body").select("#mainDiv")
+						.select("svg")[0][0].clientHeight
+					var rectHeight = thisRect.height.baseVal.value;
+					var mult = svgHeight / rectHeight;
+					return dif * mult;
+				})
+			d3.select("body").select("#mainDiv").select("svg")
+				.append("g").attr("class", "backbutton")
+				.append("rect")
+				.attr("x", d.attr("x"))
+				.attr("y", d.attr("y"))
+				.attr("width", 0)
+				.attr("height", 0)
+				.attr("opacity", 1)
+				.style("fill", "white")
+				.classed("back", true)
+				.transition().duration(1000)
+				.attr("width", 50)
+				.attr("height", 30)
+				.attr("x", 20)
+				.attr("y", 20)
+				.each("end", function(d) {
+					d3.select(".backbutton")
+						.on("mousedown", function() {
+							zoomOut();
+						});
+				});
+				
+			d3.select(".backbutton").append("text")
+				.text("Back")
+				.attr("x", d.attr("x"))
+				.attr("y", d.attr("y"))
+				.attr("width", 0)
+				.attr("height", 0)
+				.attr("font-size", 0)
+				.transition().duration(1000)
+				.attr("font-size", 12)
+				.attr("x", 32)
+				.attr("y", 40)
+				
+			d.moveToFront;
 		}
-		if(layer == 2) {
-			
-		}
+		queue()
+			.defer(setValue)
+			.awaitAll(doAction)
+		
 	};
+	
+	function zoomOut() {
+	
+		d3.select("#tiles").selectAll("*")
+		.on("mouseover", function(d) {
+			var t = d.server.nickname + "\n" + d.topo.resp_avg;
+			d3.select("#dirtyNumbers").text(t)
+		})
+		.on("mouseout", function() {
+			d3.select("#dirtyNumbers").text("")
+		})
+		.on("click", function(d, i) {
+			d3.selectAll(".hoverable").on("mouseout", null)
+				.on("mouseover", null).on("click", null);
+			d3.select(".combobox").select("form").select("select").attr("disabled", true);
+			zoomIn(d3.select(this), i);
+			d3.select(this).attr("class", function() {
+				var re = d3.select(this).attr("class");
+				re = re.replace("hoverable", "");
+				re = re.replace(" ", "");
+				return re;
+			})
+			$(".threshold_slider").slider("option", "disabled", true);
+		})
+		d3.select(".combobox").select("form").select("select").attr("disabled", null);
+		d3.select("#tiles").selectAll("*").attr("class", function() {
+			var re = d3.select(this).attr("class");
+			if(re.indexOf("hoverable") == -1) {
+				re = "hoverable " + re;
+			}
+			return re;
+		})
+		$(".threshold_slider").slider("option", "disabled", false);
+		d3.select(".backbutton").remove()
+		d3.select(thisRect)
+			.transition().duration(1000)
+			.attr("width", 40)
+			.attr("height", 40)
+			.attr("x", oldx)
+			.attr("y", oldy);
+		d3.select("#tiles").selectAll("*")
+			.filter(function(x, j) {
+				return x != thisRect;
+			})
+			.transition().duration(1000)
+			.attr("x", function() {
+				var dif = (this.x.baseVal.value / columns)
+				return dif + oldx;
+			})
+			.attr("y", function() {
+				var dif = (this.y.baseVal.value / columns)
+				return dif + oldy;
+			})
+			.attr("width", 40)
+			.attr("height", 40)
+	}
 
-	d3.select("body").select("#mainDiv")		
+	d3.select("body").select("#mainDiv")	
 		.append("div")
 		.attr("id", "bellsnwhistles")
 	d3.select("body").select("#mainDiv")
@@ -155,43 +262,47 @@ function createTiles(s) {
 		.attr("width", 600)
 		.attr("height", 600)
 	d3.select("body").select("#mainDiv").select("svg")
-	.append("g")
-	.selectAll("rect")
-	.data(s)
-	.enter()
-	.append("rect")
-	.attr("x", function(d, i) {
-		return 40 * (i % columns); //first number is width of rect, second is how many in a row
-	})
-	.attr("y", function(d, i) {
-		return Math.floor(i / columns) * 40; //first number is how many in a row, second is width
-	})
-	.attr("width", 40)
-	.attr("height", 40)
-	.on("click", function(d, i) {
-		zoom(d3.select(this), i)
-		d3.select(this).attr("class", function() {
-			var re = d3.select(this).attr("class");
-			re = re.replace("hoverable", "");
-			re = re.replace(" ", "");
-			return re;
+		.append("g").attr("id", "tiles")
+		.selectAll("rect")
+		.data(s)
+		.enter()
+		.append("rect")
+		.attr("x", function(d, i) {
+			return 40 * (i % columns);
 		})
-		$(".threshold_slider").slider("option", "disabled", true);
-	})
-	.on("mouseover", function(d) {
-		var t = d.server.nickname;
-		d3.select("#dirtyNumbers").text(t)
-	})
-	.on("mouseout", function() {
-		d3.select("#dirtyNumbers").text("")
-	});
+		.attr("y", function(d, i) {
+			return Math.floor(i / columns) * 40;
+		})
+		.attr("width", 40)
+		.attr("height", 40)
+		.on("click", function(d, i) {
+			d3.selectAll(".hoverable").on("mouseout", null)
+				.on("mouseover", null).on("click", null);
+			d3.select(".combobox").select("form").select("select").attr("disabled", true);
+			zoomIn(d3.select(this), i)
+			d3.select(this).attr("class", function() {
+				var re = d3.select(this).attr("class");
+				re = re.replace("hoverable", "");
+				re = re.replace(" ", "");
+				return re;
+			})
+			$(".threshold_slider").slider("option", "disabled", true);
+		})
+		.on("mouseover", function(d) {
+			var t = d.server.nickname + "\n" + d.topo.resp_avg;
+			d3.select("#dirtyNumbers").text(t)
+		})
+		.on("mouseout", function() {
+			d3.select("#dirtyNumbers").text("")
+		});
 	
 	var createSlider = function(callback) {
 		d3.select("#bellsnwhistles")
-		.append("div").classed("slider_title", true)
-		.append("p").text("Threshold")
-		d3.select("#bellsnwhistles")
-		.append("div").classed("threshold_slider", true)
+			.append("div").attr("id", "threshold_slider")
+			.append("div").classed("slider_title", true)
+			.append("p").text("Threshold")
+		d3.select("#threshold_slider")
+			.append("div").classed("threshold_slider", true)
 		$(function() {
 			$(".threshold_slider").slider({
 			  slide: function( event, ui ) { 
@@ -218,7 +329,6 @@ function createTiles(s) {
 					}
 				});
 			  },
-			  // animate: "fast",
 			  step: 10000,
 			  min: 0,
 			  max: 2000000,
@@ -226,12 +336,25 @@ function createTiles(s) {
 			});
 			callback(null, $(".threshold_slider").slider("option", "value"));
 		});
-		d3.select("#bellsnwhistles")
+		d3.select("#threshold_slider")
 			.append("div").attr("id", "slider_value")
-			.append("p").text("1.00 s")
+			.text("1.00 s")
 	}
 	
-	var adjustColors = function(callback) {
+	function createComboBox() {
+	
+		var options = [ "", "avg_resp_time", "name" ];
+		var combobox = d3.select("#bellsnwhistles").append("div").classed("combobox", true)
+			combobox.append("div").classed("combolabel", true).text("Sort: ")
+			combobox.append("form").attr("name", "combobox")
+			.append("select").attr("name", "selection").attr("size", 1).attr("onChange", "filterServers()")
+			options.forEach( function(e) {
+				d3.select(".combobox").select("form").select("select")
+					.append("option").attr("value", e).text(e);
+			});
+	}
+	
+	var adjustColors = function(number, callback) {
 		d3.select("body").select("#mainDiv").select("svg")
 			.select("g")
 			.selectAll("rect")
@@ -239,10 +362,9 @@ function createTiles(s) {
 				if(d.topo.id != "none") {
 					if(typeof d.topo.resp_avg != 'undefined') {
 						var quantize = d3.scale.quantile()
-							.domain([0, $(".threshold_slider")
-							.slider("option", "value")])
+							.domain([0, number])
 							.range(d3.range(10));
-						return "hoverable q" + (1 + quantize(d.topo.resp_avg))
+						return "hoverable h" + (1 + quantize(d.topo.resp_avg))
 					}
 					else {
 						return "hoverable"
@@ -252,14 +374,19 @@ function createTiles(s) {
 					return "notdefined"
 				}
 			});
+		if(typeof callback != "undefined") {
+			callback();
+		}
 	}
 	
 	var addDirtyNumbers = function(callback) {
 		d3.select("#bellsnwhistles").append("div").attr("id", "dirtyNumbers");
 		callback();
 	}
+	
 	queue()
 		.defer(createSlider)
-		.defer(adjustColors)
+		.defer(createComboBox)
+		.defer(adjustColors, $(".threshold_slider").slider("option", "value"))
 		.defer(addDirtyNumbers);
 }
--- src/processes.js
+++ src/processes.js
@@ -1,1 +1,75 @@
 //Going to be a prototype for the process view
+
+var mainDiv = d3.select("body").append("div").attr("id", "mainDiv");
+
+var controls = mainDiv.append("div").attr("id", "controls")
+var appcontainer = mainDiv.append("div").attr("id", "appcontainer")
+var svgContainer = appcontainer.append("svg");
+
+function parseData() {
+	var q = queue();
+	d3.json("http://localhost:9000/api/v3/servers/6324/processes/", function(error, data) {
+		data.forEach( function(d) { 
+			q.defer(makeObject, d, "http://localhost:9000/api/v3/processes/" + d.uid + "/data/")
+		})
+		q.awaitAll(function(err, d) { 
+			var finish = new Date();
+			var difference = new Date();
+			difference.setTime(finish.getTime() - start.getTime());
+			console.log( difference.getSeconds() + "." + difference.getMilliseconds() );
+			showProcesses(d);
+		})
+	})
+	
+	function makeObject(d, url, callback) {
+		d3.json(url, function(error, data) {
+			callback( null, { init: d, data: data } );
+		})
+	}
+}
+
+var start = new Date();
+parseData()
+	
+function filterProcesses() {
+	var text = d3.select("#textBox")[0][0].value;
+	d3.select("#procs").selectAll("*")
+		.filter(function() {
+			return d3.select(this)[0][0].__data__.init.name.indexOf(text) == -1;
+		})
+		.transition().duration(500).attr("opacity", 0);
+	d3.select("#procs").selectAll("*")
+		.filter(function() {
+			return d3.select(this)[0][0].__data__.init.name.indexOf(text) != -1;
+		})
+		.transition().duration(500).attr("opacity", 1);
+}
+	
+function showProcesses(d) {
+	var procs = svgContainer.append("g").attr("id", "procs");
+	controls.append("input").attr("id", "textBox").attr("type", "text").attr("oninput", "filterProcesses()");
+	procs.selectAll("circle").data(d).enter()
+		.append("circle")
+		.attr("r", 0)
+		.style("fill", function() {
+			var r = Math.floor(Math.random() * 3);
+			if(r == 0) {
+				return "blue"
+			} else if(r == 1) {
+				return "red"
+			} else {
+				return "yellow"
+			}
+		})
+		.attr("stroke", "black")
+		.attr("stroke-width", 1)
+		.transition().duration(1000)
+		.ease("elastic")
+		.attr("r", 20)
+		.attr("cx", function(x, i) {
+			return 50 * (i % 10) + 30;
+		})
+		.attr("cy", function(x, i) {
+			return 50 * Math.floor(i / 10) + 30;
+		})
+}
